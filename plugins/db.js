93c1a4aee246816c9d8fff3159ebe7fe04c6035f
aizwellenstan <aizwellenstan@gmail.com>
2019-08-15 01:33:44 +0900

    add env

--- plugins/db.js
+++ plugins/db.js
@@ -1,6 +1,7 @@
 module.exports = app => {
+    require('dotenv').config();
     const mongoose = require('mongoose')
-    mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/blog_db', {
+    mongoose.connect(process.env.MONGODB_URI , {
         useNewUrlParser: true
     }).then(()=>{
         console.log('db connected')
