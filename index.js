f50b5cc82cfa18693e58579ffb0d850b1b73df7d
aizwellenstan <aizwellenstan@gmail.com>
2019-08-14 23:12:18 +0900

    mongodb for heroku

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const serveStatic = require("serve-static")
+const path = require('path')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(express.static(__dirname + "/dist/"))
+app.use(serveStatic(path.join(__dirname, 'dist')));
 
 app.set('secret', 'abcd')
 app.use(require('cors')())
