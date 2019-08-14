334752a6a0243595de34dddcf4724de8f2ca63ca
aizwellenstan <aizwellenstan@gmail.com>
2019-08-14 22:18:01 +0900

    Initial Commit

--- index.js
+++ index.js
@@ -2,18 +2,10 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-// どこにアクセスさせるかの設定
-// distフォルダ：コンパイルされたファイルが入る場所
-// app.use(express.static(__dirname + "/dist/"));
-
-// SPAの時、ルーティングがうまくいかない時があるので以下の設定
-// これをしていないと、https://~~~/aboutとかに行った時にリロードするとエラーがでる
-// SPAだとaboutファイルを直で読み込んでいないから
-// これでルート以外でリロードしてもindex.htmlを読み込んでちゃんとルーティングをしてくれる
-// /.*/で全てのルートを指定。req(request), res(response)
-//app.get(/.*/, function(req, res) {
-//    res.sendFile(__dirname + "/dist/index.html");
-//});
+app.use(express.static(__dirname + "/dist/"))
+app.get(/.*/, function(req, res){
+  res.sendFile(__dirname+"/dist/inde.html");
+})
 
 app.set('secret', 'abcd')
 app.use(require('cors')())
