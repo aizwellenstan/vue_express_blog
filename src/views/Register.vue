27104cc22a696c888fb2e759ca3b644a6a6a2c39
aizwellenstan <aizwellenstan@gmail.com>
2019-08-14 19:53:27 +0900

    add Procfile

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,21 +1,25 @@
 <template>
       <div class="register-container">
-        <el-card header="Please Register" class="register-card text-primary fs-xl">
+        <div style="text-align:center; padding-top:2%">
+          <h1>ようこそ!</h1>
+          <p>新規登録(無料)して利用を開始しましょう。</p>
+        </div>
+        <el-card header="" class="register-card text-primary fs-xl">
             <el-form @submit.native.prevent="register">
-                <el-form-item label="User Name">
+                <el-form-item label="ユーザ名">
                     <el-input v-model="model.username"></el-input>
                 </el-form-item>
-                <el-form-item label="密码">
+                <el-form-item label="パスワード">
                     <el-input type="password" v-model="model.password"></el-input>
                 </el-form-item>
-                <el-form-item label="确认密码">
+                <el-form-item label="パスワード確認">
                     <el-input type="password" v-model="model.comfirmPassword"></el-input>
                 </el-form-item>
                 <el-form-item>
                     <div class="d-flex jc-between">
-                        <el-button type="primary" native-type="submit" class="btn">注册</el-button>
-                        <a href="/login" class="text-primary fs-sm">已有账号，去登录</a>
+                        <el-button type="primary" native-type="submit" class="btn">登録</el-button>
                     </div>
+                    <p style="color:#999">もしカウントを持っている場合は<a href="/login" style="color:#337ab7">ログイン</a>から。</p>
 
                 </el-form-item>
             </el-form>
