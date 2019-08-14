4e10c1097dc7558bfc4b2766d497f7e8a83ad7ad
aizwellenstan <aizwellenstan@gmail.com>
2019-08-15 02:25:32 +0900

    add mongodb auto reconnect

--- plugins/db.js
+++ plugins/db.js
@@ -2,6 +2,7 @@ module.exports = app => {
     require('dotenv').config();
     const mongoose = require('mongoose')
     mongoose.connect(process.env.MONGODB_URI , {
+        server: { auto_reconnect: true },
         useNewUrlParser: true
     }).then(()=>{
         console.log('db connected')
