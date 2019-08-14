413342d3ef68a87c2ba52570a12a74f632097d09
aizwellenstan <aizwellenstan@gmail.com>
2019-08-14 20:10:37 +0900

    add Procfile

--- index.js
+++ index.js
@@ -2,7 +2,18 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+// どこにアクセスさせるかの設定
+// distフォルダ：コンパイルされたファイルが入る場所
 app.use(express.static(__dirname + "/dist/"));
+
+// SPAの時、ルーティングがうまくいかない時があるので以下の設定
+// これをしていないと、https://~~~/aboutとかに行った時にリロードするとエラーがでる
+// SPAだとaboutファイルを直で読み込んでいないから
+// これでルート以外でリロードしてもindex.htmlを読み込んでちゃんとルーティングをしてくれる
+// /.*/で全てのルートを指定。req(request), res(response)
+app.get(/.*/, function(req, res) {
+    res.sendfile(__dirname + "/dist/index.html");
+});
 
 app.set('secret', 'abcd')
 app.use(require('cors')())
