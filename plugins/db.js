449a1410305e48ff599705658e5a9f93775f7498
aizwellenstan <aizwellenstan@gmail.com>
2019-08-14 22:28:13 +0900

    mongodb for heroku

--- plugins/db.js
+++ plugins/db.js
@@ -1,6 +1,6 @@
 module.exports = app => {
     const mongoose = require('mongoose')
-    mongoose.connect('mongodb://localhost:27017/blog_db', {
+    mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/blog_db', {
         useNewUrlParser: true
     }).then(()=>{
         console.log('db connected')
